a46f6a3a873f824238596d270343ba52bd87db87
Jakob Odersky <jodersky@gmail.com>
2013-07-08 13:12:50 +0200

    decrement major version to 0

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ import JniKeys._
 
 object FlowBuild extends Build {
   val Organization = "com.github.jodersky"
-  val Version = "1.0-SNAPSHOT" //version of flow library
+  val Version = "0.1-SNAPSHOT" //version of flow library
   val BinaryMajorVersion = 1 //binary major version used to select so's and dlls when publishing (needs to be incremented if API changes are made to flow.h or NativeSerial.java)
   val ScalaVersion = "2.10.1"
   //see native settings down below
