fa9ab8933f1452cc43724b5067d78b55d1969ceb
Jakob Odersky <jodersky@gmail.com>
2013-07-09 16:06:13 +0200

    update akka dependency to version 2.2.0 final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  lazy val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.2.0-RC2"
+  lazy val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.2.0"
   
   lazy val ioCore = "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.2"
   lazy val ioFile = "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.2"
