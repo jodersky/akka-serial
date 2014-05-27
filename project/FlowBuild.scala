817cc1750df9064dba5ebe090aea43daded02120
Chip Collier <photex@gmail.com>
2014-05-27 16:19:05 -0700

    updated to use scala 2.11.1
    [jodersky@gmail.com: remove extra dependencies to parserCombinators]

--- project/FlowBuild.scala
+++ project/FlowBuild.scala
@@ -7,7 +7,7 @@ import NativeKeys._
 
 object FlowBuild extends Build {
   val Organization = "com.github.jodersky"
-  val ScalaVersion = "2.10.4"
+  val ScalaVersion = "2.11.1"
   val Version = "2.0.1"
   
   
