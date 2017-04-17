bfe0eaf09c23116af4bf610ecb4fceb9e26b640b
Jakob Odersky <jakob@odersky.com>
2017-04-17 15:39:02 -0700

    Upgrade Akka to 2.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,10 +4,10 @@ import sbt._
 
 object Dependencies {
 
-  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.4.16"
-  val akkaStream ="com.typesafe.akka" %% "akka-stream" % "2.4.16"
+  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.5.0"
+  val akkaStream ="com.typesafe.akka" %% "akka-stream" % "2.5.0"
 
-  val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % "2.4.16"
+  val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % "2.5.0"
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.1"
 
 }
